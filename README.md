6fa73e78fada7f254967e57abb3f278cf9cfd016
Franklin van Nes <franklin.vannes@gmail.com>
2022-02-04 11:05:39 -0500

    Showcase work in README

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # Sketches
 
-The subfolders featured in this project are each individual sketches written in Rust and use the nannou project.
+## Motivation
+
+I have followed generative artists like [Ben Kovach](https://bendotk.com/) and [Tyler Hobbs](https://tylerxhobbs.com/work) for a while, and their artwork is truly impressive. Check them out! Similarly, the [/r/generative](https://www.reddit.com/r/generative) community produces some stunning artwork! I encourage everyone who codes to try out some generative art programming, and this project is my own exploration of creating generative art. I also used this as an opportunity to feel more comfortable with Rust and to try and make something cool. As a result, my code is quite ugly and explorative; you will see lots of commented out and unoptmized code - sorry!
+
+The subfolders featured in this project are each individual sketches written in Rust and use the nannou project for drawing.
 
 To run a particular sketch, simply run `cargo run --release -p <sketch_name>`, like: `cargo run --release -p cells`.
+
+## Result
+
+While these sketches don't come close to the level of quality shown by some of the generative artists out there, I never expect it to! Despite that, I was still able explore a couple of generative art techniques like flow fields, weighted color palettes, mesh generation and animation. I was also able to build my understanding of the Rust language in a really fun way - art!
+
+Without further ado, here are some pieces that my code and I produced:
+
+**Ten Thousand Triangle**
+![Ten Thousand Triangles](/examples/tenthousand-art.png)
+
+**Cells 1**
+![Cells 1](/examples/16x9_offwhite.png)
+
+**Weighted Cell Fill**
+![Weighted Cell Fill](/examples/randomized-fill.png)
+
+**Cells 4**
+![Cells 4](/examples/cells_4.png)
+
+**Mesh Cells**
+![Mesh Cells](/examples/big-block-big-mesh.png)
+
+**Connect Random Points Fill**
+![Connect Random Points Fill](/examples/random-point-fill.png)
--- tenthousand/src/main.rs
+++ tenthousand/src/main.rs
@@ -1,6 +1,7 @@
 use nannou::color::rgb_u32;
-use nannou::noise::{NoiseFn, Perlin};
+use nannou::noise::{NoiseFn, Perlin, Seedable};
 use nannou::prelude::*;
+use nannou::rand::{prelude::SliceRandom, thread_rng};
 use std::f64::consts::PI;
 
 const MAX_RECORDABLE_FRAMES: u64 = 1;
@@ -21,6 +22,14 @@ const MAX_RECORDABLE_FRAMES: u64 = 1;
 //     ((0xFF, 0xFF, 0xFF), 0.00),
 // ];
 
+// Sepia colors
+const WEIGHTED_COLORS: [((u8, u8, u8), f32); 4] = [
+    ((0x9E, 0x7D, 0x43), 0.05),
+    ((0x5C, 0x48, 0x27), 0.6),
+    ((0xDB, 0xAD, 0x5C), 0.15),
+    ((0xC2, 0x99, 0x51), 0.2),
+];
+
 // const WEIGHTED_COLORS: [((u8, u8, u8), f32); 5] = [
 //     ((0x97, 0xBD, 0xC2), 0.05),
 //     ((0x45, 0x86, 0x8F), 0.6),
@@ -29,7 +38,7 @@ const MAX_RECORDABLE_FRAMES: u64 = 1;
 //     ((0xC2, 0x97, 0xAC), 0.00),
 // ];
 
-const BACKGROUND: u32 = 0xFDF9F5;
+const BACKGROUND: u32 = 0xE8B761;
 
 fn main() {
     env_logger::init();
@@ -38,26 +47,66 @@ fn main() {
 
 type Triangle = geom::Tri<[f32; 2]>;
 
+struct Mesh {
+    triangles: Vec<Triangle>,
+    color: Srgba<u8>,
+}
+
 struct Model {
     // Art fields BELOW
-    triangles: Vec<Triangle>,
-    noise: Perlin,
+    meshes: Vec<Mesh>,
 }
 
 fn model(app: &App) -> Model {
     let window = app.window_rect();
-    let bigger_rect = Rect::from_xy_wh(window.xy(), Vec2::new(window.w() * 3f32, window.h() * 3f32));
-    
-    // create subdivisions from window rect.
-    let triangles: Vec<Triangle> = subtriangles(&bigger_rect, 7);
+
+    let mut meshes = Vec::new();
 
     // use a reusable perlin noise map which update() will move the triangles over.
-    let perlin = Perlin::new();
+    for i in 0..100 {
+        let xy = Vec2::new(
+            random_range::<f32>(window.left(), window.right()),
+            random_range::<f32>(window.bottom(), window.top()),
+        );
+        let wh = Vec2::new(
+            random_range::<f32>(window.w() / 4f32, window.w()),
+            random_range::<f32>(window.h() / 4f32, window.h()),
+        );
+        let rect = Rect::from_xy_wh(xy, wh);
+        let mut rng = thread_rng();
 
-    Model {
-        triangles,
-        noise: perlin,
+
+        // create subdivisions from window rect.
+        let mut triangles = subtriangles(&rect, random_range(4, 6));
+        // triangles.shuffle(&mut rng);
+        triangles = triangles.into_iter().take(10_000 / 100).collect();
+
+        // Shift the triangles using the perlin noise for multiple iterations.
+        let perlin = Perlin::new().set_seed(i as u32);
+        let num_iterations = random_range::<i32>(100, 250);
+        for _ in 0..num_iterations {
+            triangles = triangles
+                .iter()
+                .map(|tri| noise_shifted_triangle(tri, &perlin))
+                .collect()
+        }
+
+        // Pick an rgba color for the mesh
+        let color_bytes = WEIGHTED_COLORS
+            .choose_weighted(&mut rng, |item| item.1)
+            .unwrap()
+            .0;
+        let color = nannou::color::srgba(
+            color_bytes.0,
+            color_bytes.1,
+            color_bytes.2,
+            random_range::<u8>(1, 255),
+        );
+        let mesh = Mesh { triangles, color };
+        meshes.push(mesh);
     }
+
+    Model { meshes }
 }
 
 fn subtriangles(rect: &Rect, levels: i32) -> Vec<Triangle> {
@@ -81,50 +130,60 @@ fn convert_ratio_to_heading(ratio: f64) -> Vec2 {
     heading_vector
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    // shift every point in the angle of the noise.
-    model.triangles = model
-        .triangles
-        .iter()
-        .map(|tri| {
-            let f32_verts: [[f32; 2]; 3] = tri.0;
-            let verts: [[f64; 2]; 3] = [
-                [f32_verts[0][0] as f64, f32_verts[0][1] as f64],
-                [f32_verts[1][0] as f64, f32_verts[1][1] as f64],
-                [f32_verts[2][0] as f64, f32_verts[2][1] as f64],
-            ];
-            let heading_a = convert_ratio_to_heading(model.noise.get([verts[0][0] * 0.001f64, verts[0][1] * 0.001f64, 0.04213f64]));
-            let heading_b = convert_ratio_to_heading(model.noise.get([verts[1][0] * 0.001f64, verts[1][1] * 0.001f64, 0.04213f64]));
-            let heading_c = convert_ratio_to_heading(model.noise.get([verts[2][0] * 0.001f64, verts[2][1] * 0.001f64, 0.04213f64]));
-            return geom::Tri::from_index_tri(&[
-                [
-                    f32_verts[0][0] + heading_a.x as f32,
-                    f32_verts[0][1] + heading_a.y as f32,
-                ],
-                [
-                    f32_verts[1][0] + heading_b.x as f32,
-                    f32_verts[1][1] + heading_b.y as f32,
-                ],
-                [
-                    f32_verts[2][0] + heading_c.x as f32,
-                    f32_verts[2][1] + heading_c.y as f32,
-                ],
-            ], &[0,1,2]);
-        })
-        .collect();
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn noise_shifted_triangle(tri: &geom::Tri<[f32; 2]>, noise: &Perlin) -> geom::Tri<[f32; 2]> {
+    let f32_verts: [[f32; 2]; 3] = tri.0;
+    let verts: [[f64; 2]; 3] = [
+        [f32_verts[0][0] as f64, f32_verts[0][1] as f64],
+        [f32_verts[1][0] as f64, f32_verts[1][1] as f64],
+        [f32_verts[2][0] as f64, f32_verts[2][1] as f64],
+    ];
+    let heading_a = convert_ratio_to_heading(noise.get([
+        verts[0][0] * 0.001f64,
+        verts[0][1] * 0.001f64,
+        0.04213f64,
+    ]));
+    let heading_b = convert_ratio_to_heading(noise.get([
+        verts[1][0] * 0.001f64,
+        verts[1][1] * 0.001f64,
+        0.04213f64,
+    ]));
+    let heading_c = convert_ratio_to_heading(noise.get([
+        verts[2][0] * 0.001f64,
+        verts[2][1] * 0.001f64,
+        0.04213f64,
+    ]));
+    return geom::Tri::from_index_tri(
+        &[
+            [
+                f32_verts[0][0] + heading_a.x as f32,
+                f32_verts[0][1] + heading_a.y as f32,
+            ],
+            [
+                f32_verts[1][0] + heading_b.x as f32,
+                f32_verts[1][1] + heading_b.y as f32,
+            ],
+            [
+                f32_verts[2][0] + heading_c.x as f32,
+                f32_verts[2][1] + heading_c.y as f32,
+            ],
+        ],
+        &[0, 1, 2],
+    );
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(rgb_u32(BACKGROUND));
-
-    let triangles = &model.triangles;
-    triangles.into_iter().for_each(|tri| {
-        draw.polyline()
-            .color(BLACK)
-            .weight(0.5f32)
-            .points(tri.to_vec());
-    });
+    for mesh in &model.meshes {
+        for triangles in &mesh.triangles {
+            draw.polyline()
+                .color(mesh.color)
+                .weight(1.5f32)
+                .points(triangles.to_vec());
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 
